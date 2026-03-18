0d14e67b4cb569c447dc2b766db5db25fe09e7b8
vtecftwy <github@procurasia.com>
2026-03-18 15:44:36 +0800

    migration missed file

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from pkg_resources import parse_version
 from configparser import ConfigParser
 import setuptools, shlex
-assert parse_version(setuptools.__version__)>=parse_version('36.2')
+# Commented to fix installation problem
+# from pkg_resources import parse_version
+# assert parse_version(setuptools.__version__)>=parse_version('36.2')
 
 # note: all settings are in settings.ini; edit there, not here
 config = ConfigParser(delimiters=['='])
